57beff0fe4b9fa83961e76752688d7afc7b222e5
Ziya Suzen <ziya@suzen.net>
2025-04-09 09:46:33 +0100

    init

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NATS.Net;
+using Xunit.Abstractions;
+
+namespace TestProject1;
+
+public class UnitTest1(ITestOutputHelper outputHelper)
+{
+    [Fact]
+    public async Task Test1()
+    {
+        await using var client = new NatsClient();
+        var rtt = await client.PingAsync();
+        outputHelper.WriteLine($"rtt {rtt}");
+    }
+}
